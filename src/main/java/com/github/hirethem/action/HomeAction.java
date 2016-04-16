730525f0339b2e57fb7c8c7c5e1971e21d53bf84
Egor Shulga <egorsshulga@gmail.com>
2016-04-16 09:37:12 +0300

    Angular+Struts2 skeleton app.

---
+++ src/main/java/com/github/hirethem/action/HomeAction.java
@@ -0,0 +1,20 @@
+package com.github.hirethem.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by egors.
+ */
+public class HomeAction extends ActionSupport {
+
+    public static final String MESSAGE = "hello.message";
+
+    public String execute() {
+        return SUCCESS;
+    }
+
+    public String getMessage() {
+        return getText(MESSAGE);
+    }
+
+}
