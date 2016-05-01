9a8c6cdd2e102eaaec75af61f12a7428201bd918
Egor Shulga <egorsshulga@gmail.com>
2016-05-01 11:45:07 +0300

    Implement WorkExperience CRUD

---
+++ src/main/java/com/github/hirethem/model/dao/WorkExperienceDao.java
@@ -0,0 +1,57 @@
+package com.github.hirethem.model.dao;
+
+import com.github.hirethem.model.entity.WorkExperience;
+import com.github.hirethem.model.entity.Resume;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * Created by egorshulga on 01-May-16.
+ */
+public class WorkExperienceDao extends HibernateDao {
+
+    public void addWorkExperience(Resume resume, WorkExperience workExperience) {
+        session.beginTransaction();
+
+        resume.getWorkExperiences().add(workExperience);
+
+        session.getTransaction().commit();
+    }
+
+    public void deleteWorkExperience(int workExperienceId) {
+        session.beginTransaction();
+        Query query = session.createQuery("delete WorkExperience where id = :workExperienceId");
+        query.setParameter("workExperienceId", workExperienceId);
+        int result = query.executeUpdate();
+        session.getTransaction().commit();
+    }
+
+    public void modifyWorkExperience(int workExperienceId, String companyName, String position,
+                                     Date startDate, Date endDate, String description) {
+        session.beginTransaction();
+
+        WorkExperience workExperience = session.get(WorkExperience.class, workExperienceId);
+        workExperience.setCompanyName(companyName);
+        workExperience.setPosition(position);
+        workExperience.setStartDate(startDate);
+        workExperience.setEndDate(endDate);
+        workExperience.setDescription(description);
+
+        session.getTransaction().commit();
+    }
+
+    public WorkExperience getWorkExperience(int workExperienceId) {
+        return session.get(WorkExperience.class, workExperienceId);
+    }
+
+    public List<WorkExperience> getWorkExperiences(int resumeId) {
+        Criteria criteria = session.createCriteria(WorkExperience.class);
+        criteria.add(Restrictions.eq("id", resumeId));
+        return criteria.list();
+    }
+    
+}
---
+++ src/main/java/com/github/hirethem/model/service/WorkExperienceService.java
@@ -0,0 +1,55 @@
+package com.github.hirethem.model.service;
+
+import com.github.hirethem.model.dao.WorkExperienceDao;
+import com.github.hirethem.model.entity.WorkExperience;
+import com.github.hirethem.model.entity.Resume;
+import com.github.hirethem.model.service.exception.ServiceException;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * Created by egorshulga on 01-May-16.
+ */
+public class WorkExperienceService {
+
+    private ResumeService resumeService = new ResumeService();
+
+    private WorkExperienceDao workExperienceDao = new WorkExperienceDao();
+
+    public void createWorkExperience(int resumeId, String companyName, String position,
+                                     Date startDate, Date endDate, String description) throws ServiceException {
+        WorkExperience workExperience = new WorkExperience();
+        workExperience.setCompanyName(companyName);
+        workExperience.setPosition(position);
+        workExperience.setStartDate(startDate);
+        workExperience.setEndDate(endDate);
+        workExperience.setDescription(description);
+
+        Resume resume = resumeService.getResume(resumeId);
+
+        workExperience.setResume(resume);
+
+        workExperienceDao.addWorkExperience(resume, workExperience);
+    }
+
+    public void modifyWorkExperience(int resumeId, String companyName, String position,
+                                     Date startDate, Date endDate, String description) {
+        workExperienceDao.modifyWorkExperience(resumeId, companyName, position,
+                startDate, endDate, description);
+    }
+
+    public WorkExperience getWorkExperience(int workExperienceId) {
+        return workExperienceDao.getWorkExperience(workExperienceId);
+    }
+
+    public List<WorkExperience> getVacancies(int userId) {
+        return workExperienceDao.getWorkExperiences(userId);
+    }
+
+    public void deleteWorkExperience(int workExperienceId) {
+        workExperienceDao.deleteWorkExperience(workExperienceId);
+    }
+
+
+}
